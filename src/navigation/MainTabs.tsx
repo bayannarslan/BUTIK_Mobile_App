932484e2dc11cb12183d9f0786de77b6862bd5cb
bayannarslan <arslan.bayann@gmail.com>
2023-02-25 13:26:56 -0500

    added more to tab navigation

--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -3,6 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import type { MainTabsParamList } from './types';
 
 import Home from '../screens/Home';
+import Cart from '../screens/Cart';
+import QRcode from '../screens/QRcode';
+import Likes from '../screens/Likes';
 import MyButik from '../screens/MyButik';
 
 const Tabs = createBottomTabNavigator<MainTabsParamList>();
@@ -11,6 +14,9 @@ const MainTabs = () => {
   return (
     <Tabs.Navigator>
       <Tabs.Screen name="Home" component={Home} />
+      <Tabs.Screen name="Cart" component={Cart} />
+      <Tabs.Screen name="QRcode" component={QRcode} />
+      <Tabs.Screen name="Likes" component={Likes} />
       <Tabs.Screen name="MyButik" component={MyButik} />
     </Tabs.Navigator>
   );
---
+++ src/screens/Likes.tsx
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { MainStackParamList, MainTabsParamList } from '../navigation/types';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { supabase } from '../supabase';
+import { AuthContext } from '../provider/AuthProvider';
+import { CompositeScreenProps } from '@react-navigation/native';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+
+export type MyProfileProps = CompositeScreenProps<
+  BottomTabScreenProps<MainTabsParamList, 'Home'>,
+  NativeStackScreenProps<MainStackParamList>
+>;
+
+// type Props = NativeStackScreenProps<MainTabsParamList, 'Home'>;
+
+const Home: React.FC<MyProfileProps> = ({ navigation }) => {
+  const auth = useContext(AuthContext);
+  const userId = auth.session?.user!.id;
+  const [users, setUsers] = useState<any>([]);
+  // get all the users
+  useEffect(() => {
+    // console.log(auth.session?.access_token);
+    const fetcher = async () => {
+      // await supabase.auth.signOut();
+      const user = await supabase
+        .from('profile')
+        .select('*')
+        .filter('user_id', 'not.eq', userId);
+      if (!user.data) {
+        throw new Error('what?!');
+      }
+      setUsers(user.data);
+    };
+    fetcher();
+  }, []);
+
+  const productList = [
+    {
+      name: "Purse",
+      price: "100"
+    },
+    {
+      name: "Shoes",
+      price: "299"
+    }
+  ]
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text>This is the Home tab</Text>
+      {productList .map((product: any) => {
+        return (
+          <Pressable
+            key={product.name}
+            style={{
+              padding: 25,
+              margin: 10,
+              backgroundColor: '#ebebeb',
+              width: '75%'
+            }}
+          >
+            <Text>{product.name}</Text>
+            <Text>{product.price}</Text>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+};
+
+export default Home;
